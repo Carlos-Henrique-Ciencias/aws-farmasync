5a3b587b6f81feee14d2787521363ed16ba34692
Carlos-Henrique-Ciencias <halo2mega@outlook.com>
2024-06-25 13:23:13 -0300

    Initial commit with farmasync-backend and farmasyncweb

--- aws-farmasync/src/routes/appointmentRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/appointmentRoutes.js
-const express = require('express');
-const router = express.Router();
-const appointmentController = require('../controllers/appointmentController');
-
-router.post('/create', appointmentController.createAppointment);
-router.get('/get', appointmentController.getAppointments);
-
-module.exports = router;
