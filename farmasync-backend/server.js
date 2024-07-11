168e3efc217550e8f7b7118fea243a562fbbbbb0
Carlos-Henrique-Ciencias <halo2mega@outlook.com>
2024-07-11 13:45:52 -0300

    mandando td do aws pro vercel

--- farmasync-backend/server.js
+++ farmasync-backend/server.js
@@ -68,7 +68,7 @@ wss.on('connection', (ws) => {
 });
 
 // Inicialização do servidor
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
